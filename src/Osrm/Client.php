2a76704204329c087b4000ddb157ca529dcfb902
Fran Álvarez <fjalvarez@gmail.com>
2016-02-16 18:16:31 +0100

    Update client

--- src/Osrm/Client.php
+++ src/Osrm/Client.php
@@ -8,6 +8,7 @@ use Osrm\Response\DrivingInstruction;
 
 class Client
 {
+    const DEFAULT_ZOOM = 18;
     protected $server;
 
     protected $hintChecksum;
@@ -26,67 +27,79 @@ class Client
 
     /**
      * The 'viaroute' implementation of the OSRM server API.
+     * @param array $coordinates
+     * @param int $zoom
+     * @param bool $compression
+     * @param bool $instructions
      * @return DrivingInstruction
-     * @throws OsrmException
-     * coordinates. Two arguments (from/to) must be provided.
+     * @throws OsrmException coordinates. Two arguments (from/to) must be provided.
      */
-    public function getRoute()
+    public function getRoute($coordinates, $zoom = self::DEFAULT_ZOOM, $compression = false, $instructions = false)
     {
         $this->prepareServerUrl();
-        $requestUrl = $this->server . 'viaroute?';
-        if (func_num_args() < 2) {
+        $requestUrl = $this->server . "viaroute?instructions=".var_export($instructions, true)."&compression=".var_export($compression, true)."&z={$zoom}";
+        if ($coordinates < 2) {
             throw new OsrmException('A minimum of two arguments must be provided.', 2);
         }
 
-        for ($j = 0; $j < func_num_args(); $j++) {
-            $coord = func_get_arg($j);
+        for ($j = 0; $j < count($coordinates); $j++) {
+            $coordinate = $coordinates[$j];
 
-            if ($coord instanceof Coordinate) {
-                if ($j != 0) {
-                    $requestUrl = $requestUrl . '&';
-                }
-                $requestUrl = $requestUrl . $coord;
+            if ($coordinate instanceof Coordinate) {
+                $requestUrl = $requestUrl . '&' . $coordinate;
             }
         }
-
         $curl = curl_init();
         curl_setopt_array($curl, array(
             CURLOPT_RETURNTRANSFER => 1,
             CURLOPT_URL => $requestUrl,
         ));
 
-        $resp = curl_exec($curl);
+        $response = curl_exec($curl);
         curl_close($curl);
 
-        $json = json_decode($resp);
+        $decodedResponse = json_decode($response, true);
 
-        if ($json->status === 0) {
+        if ($decodedResponse['status'] === 200) {
             $route = new Route();
-            $route->setEndPoint($json->route_summary->end_point)
-                ->setStartPoint($json->route_summary->start_point)
-                ->setTotalTime($json->route_summary->total_time)
-                ->setTotalDistance($json->route_summary->total_distance)
-                ->setRouteGeometry($json->route_geometry);
 
-            $instructionsJson = $json->route_instructions;
-            $instructions = array();
-            foreach ($instructionsJson as $instrObj) {
-                $instruction = new DrivingInstruction();
-                $instruction->setTurnInstruction($instrObj[0])
-                    ->setWayName($instrObj[1])
-                    ->setLength($instrObj[2])
-                    ->setPosition($instrObj[3])
-                    ->setTime($instrObj[4])
-                    ->setLengthUnit($instrObj[5])
-                    ->setDirection($instrObj[6])
-                    ->setAzimuth($instrObj[7]);
-                array_push($instructions, $instruction);
+            if (isset($decodedResponse['route_summary']['end_point'])) {
+                $route->setEndPoint($decodedResponse['route_summary']['end_point']);
             }
-            $route->setRouteInstructions($instructions);
+            if (isset($decodedResponse['route_summary']['start_point'])) {
+                $route->setStartPoint($decodedResponse['route_summary']['start_point']);
+            }
+            if (isset($decodedResponse['route_summary']['total_time'])) {
+                $route->setTotalTime($decodedResponse['route_summary']['total_time']);
+            }
+            if (isset($decodedResponse['route_summary']['total_distance'])) {
+                $route->setTotalDistance($decodedResponse['route_summary']['total_distance']);
+            }
+            if (isset($decodedResponse['route_geometry'])) {
+                $route->setRouteGeometry($decodedResponse['route_geometry']);
+            }
+
+            if (isset($decodedResponse['route_instructions'])) {
+                $instructionsJson = $decodedResponse['route_instructions'];
+                $instructions = array();
+                foreach ($instructionsJson as $instrObj) {
+                    $instruction = new DrivingInstruction();
+                    $instruction->setTurnInstruction($instrObj[0])
+                        ->setWayName($instrObj[1])
+                        ->setLength($instrObj[2])
+                        ->setPosition($instrObj[3])
+                        ->setTime($instrObj[4])
+                        ->setLengthUnit($instrObj[5])
+                        ->setDirection($instrObj[6])
+                        ->setAzimuth($instrObj[7]);
+                    array_push($instructions, $instruction);
+                }
+                $route->setRouteInstructions($instructions);
+            }
 
             return $route;
         } else {
-            throw new OsrmException("Osrm status error", $json->{'status'});
+            throw new OsrmException("Osrm status error", $decodedResponse->{'status'});
         }
     }
 
